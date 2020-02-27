b6665c5d0ee59ff84f0990365bdfafc6f28478b9
Ajeet jain <ajeet@planetscale.com>
2020-02-27 12:57:29 +0530

    My FIrst commit

---
+++ src/main/java/com/howtodoinjava/rest/model/Employee.java
@@ -0,0 +1,63 @@
+package com.howtodoinjava.rest.model;
+
+
+public class Employee {
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //Getters and setters
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", + " +
+                "lastName=" + lastName + ", email=" + email + "]";
+    }
+}
